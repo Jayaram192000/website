fe0db5b127680e61780c48e4f224b28b6c0d7cac
Jayaram192000 <118665559+Jayaram192000@users.noreply.github.com>
2025-04-21 22:41:59 +0530

    Update vite.config.ts for nginx

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 👈 This line is critical for Vercel or static hosting
+  base: '/react/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
